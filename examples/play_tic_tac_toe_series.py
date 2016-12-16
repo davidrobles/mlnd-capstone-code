f77b1698714afed393c02a3ef760af78be3bc1c4
David Robles <drobles@gmail.com>
2016-12-16 11:50:11 -0500

    Update

--- examples/play_tic_tac_toe_series.py
+++ examples/play_tic_tac_toe_series.py
@@ -1,16 +1,9 @@
-import random
 import sys
 sys.path.insert(0, '/home/drobles/projects/mlnd-capstone-code/src/')
+from players import RandPlayer
 from tictactoe import TicTacToe
-from utils import play_random_game, play_series
+from utils import play_series
 
-
-class RandPlayer(object):
-
-    def chooseMove(self, game):
-        return random.choice(game.legal_moves())
-
-
-players = [RandPlayer(), RandPlayer()]
 game = TicTacToe()
+players = [RandPlayer(), RandPlayer()]
 play_series(game, players)
