b6d3552c5312d8129cce9ad5f210eee0e599dce5
David Robles <drobles@gmail.com>
2016-12-17 18:04:40 -0500

    Tests for tic tac toe mdp

--- tests/test_tictactoe_mdp.py
+++ tests/test_tictactoe_mdp.py
@@ -1,8 +1,35 @@
 import unittest
+from games import TicTacToe
+from mdps import TicTacToeMDP
 
 
 class TestTicTacToeMDP(unittest.TestCase):
 
-    def setUp(self):
-        self.mdp = TicTacToeMDP()
+    mdp = TicTacToeMDP(None)
 
+    def setUp(self):
+        pass
+
+    def test_states(self):
+        self.assertEqual(len(self.mdp.states), 5478)
+
+    def test_reward_before_game_is_over(self):
+        cur_state = TicTacToe()
+        action = 1
+        next_state = cur_state.copy().make_move(action)
+        reward = self.mdp.reward(cur_state, action, next_state)
+        self.assertEqual(reward, 0)
+
+    def test_reward_when_game_is_over(self):
+        cur_state = TicTacToe()
+        action = 1
+        next_state = cur_state.copy().make_move(action)
+        reward = self.mdp.reward(cur_state, action, next_state)
+        self.assertEqual(reward, 0)
+
+    def test_reward_when_game_is_over_and_first_player_wins(self):
+        cur_state = TicTacToe().make_moves([1, 4, 2, 5])
+        action = 3
+        next_state = cur_state.copy().make_move(action)
+        reward = self.mdp.reward(cur_state, action, next_state)
+        self.assertEqual(reward, 0)
