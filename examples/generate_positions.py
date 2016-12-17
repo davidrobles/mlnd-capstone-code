e39cd07d6012d94cee61b79357f8a6ab3e46425c
David Robles <drobles@gmail.com>
2016-12-17 03:46:02 -0500

    Keep refactoring!

--- examples/generate_positions.py
+++ examples/generate_positions.py
@@ -9,17 +9,15 @@ from utils import play_series, ZobristHashing
 zobrist = ZobristHashing(n_positions=9, n_pieces=2)
 
 def count_positions(game, table):
-    count = 0
     h = zobrist.hash(game.board)
     if h not in table:
-        count += 1
         table.add(h)
     for move in game.legal_moves():
         new_game = game.copy()
         new_game.make_move(move)
-        count += count_positions(new_game, table)
-    return count
+        count_positions(new_game, table)
 
 game = TicTacToe()
-count = count_positions(game, set())
-print('Total game positions: {}'.format(count))
+s = set()
+count_positions(game, s)
+print('Total game positions: {}'.format(len(s)))
