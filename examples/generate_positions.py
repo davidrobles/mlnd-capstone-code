6b0a61918cccce61ca8dca9b53d5dadd01b381e0
David Robles <drobles@gmail.com>
2016-12-17 03:35:44 -0500

    Refactoring

--- examples/generate_positions.py
+++ examples/generate_positions.py
@@ -6,27 +6,21 @@ from games import TicTacToe
 from players import RandPlayer
 from utils import play_series, ZobristHashing
 
+# transp = set()
+zobrist = ZobristHashing(n_positions=9, n_pieces=2)
 
-game = TicTacToe()
-transp = set()
-n_positions = 9
-n_pieces = 2
-zobrist = ZobristHashing(9, 2)
-
-def traverse(game):
-    global transp
+def traverse(game, table):
     count = 0
     h = zobrist.hash(game.board)
-    if h not in transp:
-        count +=1
-        transp.add(h)
+    if h not in table:
+        count += 1
+        table.add(h)
     for move in game.legal_moves():
         new_game = game.copy()
         new_game.make_move(move)
-        count += traverse(new_game)
+        count += traverse(new_game, table)
     return count
 
-
-count = traverse(game)
+game = TicTacToe()
+count = traverse(game, set())
 print('Total game positions: {}'.format(count))
-
