1c8f097318450aebe8c772f42687b4fee5861338
David Robles <drobles@gmail.com>
2016-12-17 13:47:51 -0500

    Comment default_util_func

--- src/utils.py
+++ src/utils.py
@@ -30,6 +30,10 @@ def play_series(game, players, n_matches=100, alternate=False, verbose=True):
 
 
 def default_util_func(game, player):
+    '''
+    Returns the utility at the end of the game for the given player:
+    1 for win, 0 for draw, and -1 for loss
+    '''
     player_outcome = game.outcomes()[player]
     return {'W': 1.0, 'L': -1.0, 'D': 0.0}[player_outcome]
 
