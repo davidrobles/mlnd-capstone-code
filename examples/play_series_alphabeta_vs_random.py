32c3ed58d24eed03323f7b0ac1610f24e8e0df26
David Robles <drobles@gmail.com>
2016-12-17 00:06:33 -0500

    Remove path import

--- examples/play_series_alphabeta_vs_random.py
+++ examples/play_series_alphabeta_vs_random.py
@@ -1,5 +1,3 @@
-import sys
-sys.path.insert(0, '/home/drobles/projects/mlnd-capstone-code/src/')
 from games import TicTacToe
 from players import AlphaBeta, RandPlayer
 from utils import play_series
