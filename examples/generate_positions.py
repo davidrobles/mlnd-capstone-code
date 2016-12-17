58c971f08c14deadd49100004d71f09fa0fd1663
David Robles <drobles@gmail.com>
2016-12-17 03:27:27 -0500

    Update

--- examples/generate_positions.py
+++ examples/generate_positions.py
@@ -8,28 +8,25 @@ from utils import play_series, ZobristHashing
 
 
 game = TicTacToe()
-count = 0
 transp = set()
 n_positions = 9
 n_pieces = 2
 zobrist = ZobristHashing(9, 2)
 
-
-def traverse(game, depth=0):
-    global count
+def traverse(game):
     global transp
+    count = 0
     h = zobrist.hash(game.board)
     if h not in transp:
         count +=1
         transp.add(h)
-    max_depth = depth
     for move in game.legal_moves():
         new_game = game.copy()
         new_game.make_move(move)
-        max_depth = max(max_depth, traverse(new_game, depth + 1))
-    return max_depth
+        count += traverse(new_game)
+    return count
 
 
-max_depth = traverse(game)
+count = traverse(game)
 print('Total game positions: {}'.format(count))
-print('Max Depth: {}'.format(max_depth))
+
