50e9976c330d19bf24746f40b9d45dff691c47a3
David Robles <drobles@gmail.com>
2016-12-17 00:15:15 -0500

    Refactor AlphaBeta

--- src/players/alphabeta.py
+++ src/players/alphabeta.py
@@ -10,9 +10,6 @@ class AlphaBeta(object):
         self.eval_func = eval_func
         self.max_depth = max_depth
 
-    def choose_move(self, game):
-        return self._ab(game, 0, -100000000, 100000000)[0]
-
     def _ab(self, game, cur_depth, alpha, beta):
         if game.is_over() or cur_depth == self.max_depth:
             return None, self.eval_func(game, game.cur_player)
@@ -24,11 +21,18 @@ class AlphaBeta(object):
             new_depth = cur_depth + 1
             new_alpha = -beta
             new_beta = -max([alpha, best_score])
-            _, cur_score = self._ab(new_game, new_depth, new_alpha, new_beta)
-            cur_score = -cur_score
-            if cur_score > best_score:
-                best_score = cur_score
+            _, score = self._ab(new_game, new_depth, new_alpha, new_beta)
+            score = -score
+            if score > best_score:
+                best_score = score
                 best_move = move
                 if best_score >= beta:
                     return [best_move, best_score]
         return best_move, best_score
+
+    ##########
+    # Player #
+    ##########
+
+    def choose_move(self, game):
+        return self._ab(game, 0, -100000000, 100000000)[0]
