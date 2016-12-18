d67f3bcd47fdb10a7c33b1f33301c3547010027a
David Robles <drobles@gmail.com>
2016-12-18 13:01:10 -0500

    Refactor ZobristHashing

--- capstone/utils/play.py
+++ capstone/utils/play.py
@@ -14,16 +14,13 @@ def play_match(game, players, verbose=True):
             print(game)
 
 
-def play_series(game, players, n_matches=100, alternate=False, verbose=True):
+def play_series(game, players, n_matches=100, verbose=True):
     """Plays a series of 'n_matches' matches between the given players"""
     stats = {'p1win': 0, 'p2win': 0, 'draw': 0}
-    match_players = [players[0], players[1]]
     for n_match in range(n_matches):
         print('Match {}:'.format(n_match), end=' ')
         play_match(game, match_players, verbose=False)
         stats[game.outcome] += 1
         print(game.outcomes())
         game.reset()
-        if alternate:
-            match_players[0], match_players[1] = match_players[1], match_players[0]
     print(stats)
--- capstone/utils/zobrist.py
+++ capstone/utils/zobrist.py
@@ -4,7 +4,8 @@ import random
 class ZobristHashing(object):
 
     def __init__(self, n_positions, n_pieces):
-        self.table = [random.getrandbits(32) for i in range(n_positions * n_pieces)]
+        size = n_positions * n_pieces
+        self.table = [random.getrandbits(32) for i in range(size)]
         self.n_positions = n_positions
         self.n_pieces = n_pieces
 
@@ -12,6 +13,5 @@ class ZobristHashing(object):
         result = 0
         for i in range(self.n_positions):
             if board[i] != ' ':
-                piece = board[i]
-                result ^= self.table[i * self.n_pieces + piece]
+                result ^= self.table[i * self.n_pieces + board[i]]
         return result
