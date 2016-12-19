3510506c9361903cbb352cafce111d887e4da956
David Robles <drobles@gmail.com>
2016-12-18 19:41:43 -0500

    Add tests for TicTacToeMDP

--- tests/capstone/mdp/test_tictactoe_mdp.py
+++ tests/capstone/mdp/test_tictactoe_mdp.py
@@ -12,6 +12,19 @@ class TestTicTacToeMDP(unittest.TestCase):
     def test_states(self):
         self.assertEqual(len(self.mdp.states), 5478)
 
+    def test_actions(self):
+        cur_state = TicTacToe()
+        actual_actions = list(range(1, 10))
+        expected_actions = self.mdp.actions(cur_state)
+        self.assertItemsEqual(actual_actions, expected_actions)
+
+    def test_actions_at_end_of_game_are_empty(self):
+        game = TicTacToe()
+        players = [RandPlayer(), RandPlayer()]
+        play_match(game, players, verbose=False)
+        actual_n_actions = len(self.mdp.actions(game))
+        self.assertEqual(actual_n_actions, 0)
+
     def test_reward_when_game_is_not_over_is_zero(self):
         cur_state = TicTacToe()
         action = 1
@@ -33,5 +46,5 @@ class TestTicTacToeMDP(unittest.TestCase):
     def test_terminal_state(self):
         game = TicTacToe()
         players = [RandPlayer(), RandPlayer()]
-        play_match(game, players)
+        play_match(game, players, verbose=False)
         self.assertTrue(self.mdp.is_terminal(game))
