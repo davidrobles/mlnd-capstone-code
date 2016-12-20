413c993c527b26928e5cae444e98220dabe5f3f5
David Robles <drobles@gmail.com>
2016-12-19 19:14:16 -0500

    Mark TicTacToe board as private

--- capstone/game/tictactoe.py
+++ capstone/game/tictactoe.py
@@ -26,9 +26,9 @@ class TicTacToe(Game):
     def board(self):
         b = []
         for i in range(9):
-            if self.boards[0] & (1 << i):
+            if self._boards[0] & (1 << i):
                 b.append(0)
-            elif self.boards[1] & (1 << i):
+            elif self._boards[1] & (1 << i):
                 b.append(1)
             else:
                 b.append(' ')
@@ -36,15 +36,15 @@ class TicTacToe(Game):
 
     def _check_win(self, player_idx):
         '''Returns true if there is a winning board for the given player'''
-        board = self.boards[player_idx]
+        board = self._boards[player_idx]
         return any((board & win) == win for win in TicTacToe.WINS)
 
     def __eq__(self, other):
         if self.cur_player() != other.cur_player():
             return False
-        if self.boards[0] != other.boards[0]:
+        if self._boards[0] != other._boards[0]:
             return False
-        if self.boards[1] != other.boards[1]:
+        if self._boards[1] != other._boards[1]:
             return False
         return True
 
@@ -61,7 +61,7 @@ class TicTacToe(Game):
     def copy(self):
         tic = TicTacToe()
         tic._cur_player = self._cur_player
-        tic.boards = self.boards[:]
+        tic._boards = self._boards[:]
         return tic
 
     def cur_player(self):
@@ -70,11 +70,11 @@ class TicTacToe(Game):
     def legal_moves(self):
         if any([self._check_win(pix) for pix in range(2)]):
             return []
-        legal_board = ~(self.boards[0] | self.boards[1])
+        legal_board = ~(self._boards[0] | self._boards[1])
         return [move for move in range(1, 10) if legal_board & (1 << (move - 1))]
 
     def make_move(self, move):
-        self.boards[self.cur_player()] |= (1 << (move - 1))
+        self._boards[self.cur_player()] |= (1 << (move - 1))
         self._cur_player = (self.cur_player() + 1) % 2
         return self
 
@@ -89,7 +89,7 @@ class TicTacToe(Game):
     def reset(self):
         """Restarts the game"""
         self._cur_player = 0
-        self.boards = [0, 0]
+        self._boards = [0, 0]
 
 
 class TicTacToeView(object):
