aadb9bd587e40cdc29174abaf048a7615e7e684a
David Robles <drobles@gmail.com>
2016-12-19 19:30:55 -0500

    Implement TicTacToeEnv

--- capstone/environment/environment.py
+++ capstone/environment/environment.py
@@ -7,26 +7,26 @@ class Environment(object):
     '''An environment for reinforcement learning interactions.'''
 
     @abc.abstractmethod
-    def cur_state(self):
-        '''Returns the current state'''
+    def actions(self, ):
+        '''Returns the available actions from the current state.'''
         pass
 
     @abc.abstractmethod
-    def actions(self, state):
-        '''Returns the possible actions from the given state'''
+    def cur_state(self):
+        '''Returns the current state.'''
         pass
 
     @abc.abstractmethod
     def do_action(self, action):
-        '''Performs the given action in the current state'''
-        pass
-
-    @abc.abstractmethod
-    def reset(self):
-        '''Resets the current state to the start state'''
+        '''Performs the given action in the current state.'''
         pass
 
     def is_terminal(self):
         state = self.cur_state()
         actions = self.actions(state)
         return len(actions) == 0
+
+    @abc.abstractmethod
+    def reset(self):
+        '''Resets the current state to the start state.'''
+        pass
--- capstone/environment/tictactoe.py
+++ capstone/environment/tictactoe.py
@@ -3,14 +3,20 @@ from . import Environment
 
 class TicTacToeEnv(Environment):
 
+    def __init__(self, game_mdp):
+        self.game_mdp = game_mdp
+        self.reset()
+
     def cur_state(self):
-        pass
+        return self.game.copy()
 
-    def actions(self, state):
-        pass
+    def actions(self):
+        return self.game_mdp.actions(self.game)
 
-    def do_action(self, state, action):
-        pass
+    def do_action(self, action):
+        prev = self.game.copy()
+        self.game.make_move(action)
+        return self.game_mdp.reward(prev, action, self.game)
 
     def reset(self):
-        pass
+        self.game = self.game_mdp.start_state()
