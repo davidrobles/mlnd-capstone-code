74c78c9a75b64cd61631ef0c51d13229af7f2a92
David Robles <drobles@gmail.com>
2016-12-23 09:04:09 -0500

    Create GreedyQF player

--- capstone/player/__init__.py
+++ capstone/player/__init__.py
@@ -1,3 +1,4 @@
 from .player import Player
 from .alphabeta import AlphaBeta
+from .greedyqf import GreedyQF
 from .randplayer import RandPlayer
--- examples/tictactoe-qlearning.py
+++ examples/tictactoe-qlearning.py
@@ -15,29 +15,8 @@ from capstone.algorithms import QLearning
 from capstone.environment import Environment
 from capstone.game import TicTacToe
 from capstone.mdp import GameMDP
-from capstone.player import AlphaBeta
+from capstone.player import AlphaBeta, GreedyQF
 from capstone.util import play_series
-
-
-class PolicyPlayer(object):
-
-    def __init__(self, qf):
-        self.qf = qf
-
-    def choose_move(self, game):
-        best_value = -100000
-        best_move = None
-        for move in game.legal_moves():
-            if (game, move) not in self.qf:
-                continue
-            temp_value = self.qf[(game, move)]
-            if temp_value > best_value:
-                best_value = temp_value
-                best_move = move
-        if best_move is None:
-            return random.choice(game.legal_moves())
-        return best_move
-
 
 game = TicTacToe()
 ab = AlphaBeta()
@@ -45,5 +24,5 @@ mdp = GameMDP(game, ab, 1)
 env = Environment(mdp)
 qf = {}
 QLearning(env, qf=qf, n_episodes=1000).learn()
-players = [PolicyPlayer(qf), ab]
+players = [GreedyQF(qf), ab]
 play_series(TicTacToe(), players, 100)
