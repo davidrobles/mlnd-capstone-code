684580c9fec6bb91d99f48b0ebca5524cca34365
David Robles <drobles@gmail.com>
2016-12-24 18:16:58 -0500

    Code cleanup

--- capstone/policy/greedy.py
+++ capstone/policy/greedy.py
@@ -8,15 +8,4 @@ class GreedyPolicy(Policy):
     def action(self, env, vf=None, qf=None):
         state = env.cur_state()
         actions = env.actions(state)
-        try:
-            return max([qf[(state, action)] for action in actions])
-        except Exception as e:
-            import pdb; pdb.set_trace()
-        return 0
-
-        # best_value = -100000
-        # for next_action in actions:
-        #     temp_value = qf[(state, next_action)]
-        #     if temp_value > best_value:
-        #         best_value = temp_value
-        # return best_value
+        return max([qf[(state, action)] for action in actions])
