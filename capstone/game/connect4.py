981f4accaac7e742fab479ac5ad6b2c1e319b787
David Robles <drobles@gmail.com>
2017-01-10 12:47:47 -0500

    update

--- capstone/game/connect4.py
+++ capstone/game/connect4.py
@@ -29,17 +29,12 @@ class Connect4(Game):
         self.reset()
 
     def __eq__(self, other):
-        if self._cur_player != other._cur_player:
-            return False
-        if self._boards[0] != other._boards[0]:
-            return False
-        if self._boards[1] != other._boards[1]:
-            return False
-        if self._moves != other._moves:
-            return False
-        if self._height != other._height:
-            return False
-        return True
+        return all([
+            self._cur_player == other._cur_player,
+            self._boards == other._boards,
+            self._moves == other._moves,
+            self._height == other._height
+        ])
 
     def __repr__(self):
         return Connect4View(self).render()
