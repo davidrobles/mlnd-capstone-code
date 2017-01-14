439e39cb62bacf4a02a08972aec0b1f832dac7c6
David Robles <drobles@gmail.com>
2017-01-14 15:36:12 -0800

    updates

--- capstone/util/dbload.py
+++ capstone/util/dbload.py
@@ -1,19 +1,16 @@
 from __future__ import print_function
 from capstone.game import Connect4 as C4
 
-ROWS = 6
-COLS = 7
-
 line = 'b,b,b,b,b,b,b,b,b,b,b,b,x,o,b,b,b,b,x,o,x,o,x,o,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,win'
 values = line.split(',')
 cells = values[:-1]
 outcome = values[-1]
 
-board = [[' '] * COLS for row in range(ROWS)]
+board = [[' '] * C4.COLS for row in range(C4.ROWS)]
 
 for ix, c in enumerate(cells):
-    row = C4.ROWS - 1 - (ix % ROWS)
-    col = ix / ROWS
+    row = C4.ROWS - 1 - (ix % C4.ROWS)
+    col = ix / C4.ROWS
     if c == 'x':
         board[row][col] = 'X'
     elif c == 'o':
