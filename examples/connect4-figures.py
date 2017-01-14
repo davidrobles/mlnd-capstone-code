47ac547712d2a46bd8ff8fdef8ebd2d3807bc670
David Robles <drobles@gmail.com>
2017-01-14 10:45:03 -0800

    update

---
+++ examples/connect4-figures.py
@@ -0,0 +1,12 @@
+from capstone.util.c4fig import C42PDF
+
+
+board = [[' ', ' ', '1', ' ', ' ', ' ', ' '],
+         [' ', ' ', '2', ' ', '1', ' ', ' '],
+         [' ', ' ', '2', '2', '1', '1', '1'],
+         [' ', ' ', '1', '1', '2', '2', '2'],
+         [' ', '1', '2', '2', '1', '2', '2'],
+         [' ', '2', '1', '2', '1', '2', '1']]
+full_path = '/Users/drobles/Desktop/'
+filename = 'c4'
+C42PDF(board, full_path, filename).create()
