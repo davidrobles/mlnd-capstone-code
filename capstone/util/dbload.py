18079e34bcbc65be5cdc5042b926cc067da35251
David Robles <drobles@gmail.com>
2017-01-14 15:40:38 -0800

    update

--- capstone/util/dbload.py
+++ capstone/util/dbload.py
@@ -1,4 +1,4 @@
-from __future__ import print_function
+from __future__ import division, print_function, unicode_literals
 from capstone.game import Connect4 as C4
 
 line = 'b,b,b,b,b,b,b,b,b,b,b,b,x,o,b,b,b,b,x,o,x,o,x,o,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,win'
@@ -9,10 +9,9 @@ m = {'x': 'X', 'o': 'O', 'b': '-'}
 board = [[' '] * C4.COLS for row in range(C4.ROWS)]
 
 for ix, cell in enumerate(cells):
-    row = C4.ROWS - 1 - (ix % C4.ROWS)
-    col = ix / C4.ROWS
+    row = C4.ROWS - (ix % C4.ROWS) - 1
+    col = ix // C4.ROWS
     board[row][col] = m[cell]
-
 
 b = C4(board)
 print(b)
