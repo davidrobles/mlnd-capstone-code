71f37e1e07e17998fce0c0f550bfb2f9847a2e43
David Robles <drobles@gmail.com>
2017-01-14 15:45:28 -0800

    updates

--- capstone/util/dbload.py
+++ capstone/util/dbload.py
@@ -1,17 +1,20 @@
 from __future__ import division, print_function, unicode_literals
 from capstone.game import Connect4 as C4
 
-line = 'b,b,b,b,b,b,b,b,b,b,b,b,x,o,b,b,b,b,x,o,x,o,x,o,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,win'
-values = line.split(',')
-cells = values[:-1]
-outcome = values[-1]
-m = {'x': 'X', 'o': 'O', 'b': '-'}
-board = [[' '] * C4.COLS for row in range(C4.ROWS)]
 
-for ix, cell in enumerate(cells):
-    row = C4.ROWS - (ix % C4.ROWS) - 1
-    col = ix // C4.ROWS
-    board[row][col] = m[cell]
+def load_instance(instance):
+    values = instance.split(',')
+    cells = values[:-1]
+    outcome = values[-1]
+    m = {'x': 'X', 'o': 'O', 'b': '-'}
+    board = [[' '] * C4.COLS for row in range(C4.ROWS)]
+    for ix, cell in enumerate(cells):
+        row = C4.ROWS - (ix % C4.ROWS) - 1
+        col = ix // C4.ROWS
+        board[row][col] = m[cell]
+    return C4(board), outcome
 
-b = C4(board)
-print(b)
+instance = 'b,b,b,b,b,b,b,b,b,b,b,b,x,o,b,b,b,b,x,o,x,o,x,o,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,win'
+game, outcome = load_instance(instance)
+print(game)
+print(outcome)
