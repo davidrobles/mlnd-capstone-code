a4af97f8db1d9f2089ff130a0bc29eeed1be8c16
David Robles <drobles@gmail.com>
2017-01-15 09:51:59 -0800

    Example: Evaluate C4 UCI dataset using MonteCarlo

---
+++ examples/connect4_uci_monte_carlo.py
@@ -0,0 +1,29 @@
+from __future__ import division, print_function
+from collections import Counter
+from capstone.player import MonteCarlo
+from capstone.util.c4uci import load_instance
+
+FILENAME = 'datasets/connect-4.data'
+
+mc = MonteCarlo()
+outcomes = []
+
+with open(FILENAME) as f:
+    counter = Counter()
+    for i, line in enumerate(f, 1):
+        game, outcome = load_instance(line)
+        outcomes.append(outcome)
+        if i % 1000 == 0:
+            print(i)
+
+counter = Counter(outcomes)
+print('\n---------')
+print(' Results')
+print('---------\n')
+print('total: {}'.format(len(outcomes)))
+for outcome in ['win', 'loss', 'draw']:
+    print('{outcome}: {count} ({pct:.2f}%)'.format(
+        outcome=outcome,
+        count=counter[outcome],
+        pct=((counter[outcome] / len(outcomes)) * 100)
+    ))
