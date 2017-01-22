a1c583b1368a097f41fbf583d51719b1c7133ff3
David Robles <drobles@gmail.com>
2017-01-22 14:09:12 -0800

    Remove Tic-Tac-Toe and Connect 4 figure tests

--- examples/tictactoe_figures.py
+++
@@ -1,8 +0,0 @@
-from capstone.util import tic2pdf
-
-
-board = [[' ', ' ', '1'],
-         [' ', ' ', '2'],
-         [' ', '2', '1']]
-filename = '/Users/drobles/Desktop/tic.pdf'
-tic2pdf(board, filename)
