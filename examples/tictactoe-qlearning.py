0ac1fd96fb8604dd5cff22260efdbe369b8fe0a5
David Robles <drobles@gmail.com>
2017-01-22 11:34:45 -0800

    Learn state-action values and generate pdf figures

--- examples/tictactoe-qlearning.py
+++ examples/tictactoe-qlearning.py
@@ -1,33 +1,29 @@
 '''
-A Q-Learning algorithm learns the state-action values for Tic-Tac-Toe board
-positions against a deterministic Alpha-Beta player. Aftere the values are
-learned, we use a Greedy Player to play 100 games against AlphaBeta.
-
-Since Q-learning is a model-free algorithm, i.e. it does not require a model,
-it takes environment and it does not have access to the transition and
-reward dynamics.
-
-A greedy player that uses the learned state-action values should always
-draw against Alpha-Beta assuming the Q-learning algorithm ran for a high
-number of episodes.
-
-Note that Q-learning runs to only learn the values for board-moves for
-the first player. If we want to learn this to play as the second player,
-we need to run the algorithm against using an MDP where opp_idx = 0.
+Q-Learning is used to learn the state-action values for a Tic-Tac-Toe board position against a
+deterministic Alpha-Beta player.
 '''
 from capstone.algorithms import QLearning
 from capstone.environment import Environment
 from capstone.game import TicTacToe
 from capstone.mdp import GameMDP
-from capstone.player import AlphaBeta, GreedyQF
-from capstone.util import play_series
+from capstone.player import AlphaBeta
+from capstone.util import tic2pdf
 
-board = 'XOXO-O--X'
+board = [['X', ' ', ' '],
+         ['O', 'X', ' '],
+         [' ', 'O', ' ']]
 game = TicTacToe(board)
 ab = AlphaBeta()
 mdp = GameMDP(game, ab, 1)
 env = Environment(mdp)
 qf = {}
 QLearning(env, qf=qf, n_episodes=1000).learn()
-players = [GreedyQF(qf), ab]
-play_series(TicTacToe(board), players, n_matches=100)
+tic2pdf(game.board, 'figures/tic_ql_current.pdf')
+
+for move in game.legal_moves():
+    value = qf[(game, move)]
+    print('Move: %s' % move)
+    print('Value: %s' % value)
+    new_game = game.copy().make_move(move)
+    print(new_game)
+    tic2pdf(new_game.board, 'figures/tic_ql_move_%s_value_%.4f.pdf' % (move, value))
