1a654998620297a68865e2affa68719d361bf88e
David Robles <drobles@gmail.com>
2017-02-04 12:37:46 -0500

    Add print_header util function

--- capstone/util/__init__.py
+++ capstone/util/__init__.py
@@ -1,6 +1,14 @@
+from __future__ import division, print_function
 from .aec import print_aec, str_aec
 from .c42pdf import c42pdf
 from .tic2pdf import tic2pdf
 from .eval import utility
 from .play import play_match, play_series
 from .zobrist import ZobristHashing
+
+
+def print_header(text):
+    l = len(text) + 2
+    print('-' * l)
+    print(' {} '.format(text))
+    print('-' * l, end='\n\n')
