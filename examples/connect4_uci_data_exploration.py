27f7bae504a7c49449a425f579cf7ebf67400793
David Robles <drobles@gmail.com>
2017-02-04 12:57:48 -0500

    Load UCI C4 dataset using load_uci_c4

--- examples/connect4_uci_data_exploration.py
+++ examples/connect4_uci_data_exploration.py
@@ -1,20 +1,10 @@
 from __future__ import division, print_function
 import pandas as pd
 from sklearn.linear_model import LinearRegression
-from capstone.game import Connect4 as C4
+from capstone.datasets import load_uci_c4
 from capstone.util import print_header
 
-FILENAME = 'datasets/connect-4.data'
-
-def column_name(i):
-    if i == 42:
-        return 'outcome'
-    row = chr(ord('a') + (i // C4.ROWS))
-    col = (i % C4.ROWS) + 1
-    return '{row}{col}'.format(row=row, col=col)
-
-column_names = [column_name(i) for i in range(43)]
-df = pd.read_csv(FILENAME, header=None, names=column_names)
+df = load_uci_c4()
 outcomes = df.loc[:, 'outcome']
 
 print_header('Dataset')
