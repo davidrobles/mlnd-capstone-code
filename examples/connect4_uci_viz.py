3646b35281a1ac57c4736cb4fa61328ce86f3891
David Robles <drobles@gmail.com>
2017-02-04 21:26:19 -0500

    Generate Connect 4 boards using Pandas

--- examples/connect4_uci_viz.py
+++ examples/connect4_uci_viz.py
@@ -1,22 +1,29 @@
 from __future__ import division, print_function
 import random
+from capstone.datasets import load_uci_c4
+from capstone.game import Connect4 as C4
 from capstone.util import c42pdf
-from capstone.util.c4uci import load_instance
 
-FILENAME = 'datasets/connect-4.data'
+def series_to_game(series):
+    '''Converts a Pandas Series to a Connect 4 game'''
+    cell_map = {'x': 'X', 'o': 'O', 'b': '-'}
+    board = [[' '] * C4.COLS for row in range(C4.ROWS)]
+    cells = series.iloc[:-1]
+    outcome = series.iloc[-1]
+    for ix, cell in enumerate(cells):
+        row = C4.ROWS - (ix % C4.ROWS) - 1
+        col = ix // C4.ROWS
+        board[row][col] = cell_map[cell]
+    return C4(board), outcome
 
-games = []
-outcomes = []
+# Load UCI Connect 4 dataset
+df = load_uci_c4()
 
-with open(FILENAME) as f:
-    for i, line in enumerate(f, 1):
-        game, outcome = load_instance(line)
-        outcomes.append(outcome)
-        games.append(game)
+# Select some instances of the dataset
+df = df.iloc[:10]
 
-for i in range(1, 10 + 1):
-    ix = random.randint(0, len(games) - 1)
-    game = games[ix]
-    outcome = outcomes[ix]
+# Generate pdfs of the boards
+for i, row in df.iterrows():
+    c4, outcome = series_to_game(row)
     filename = 'figures/c4_exploration_{i}_{outcome}.pdf'.format(i=i, outcome=outcome)
-    c42pdf(game.board, filename)
+    c42pdf(c4.board, filename)
