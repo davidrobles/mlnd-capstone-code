0572be1be7c1c78847d7de8d448ad8078d338845
David Robles <drobles@gmail.com>
2017-02-11 13:36:37 -0500

    Run Q-learning episode in its own method

--- capstone/algorithms/qlearning.py
+++ capstone/algorithms/qlearning.py
@@ -48,17 +48,22 @@ class QLearning(object):
         return self._best([self.qf[(state, action)] for action in actions])
 
     def learn(self):
-        for episode in range(1, self.n_episodes + 1):
-            print('Episode %d / %d)' % (episode, self.n_episodes))
-            self.env.reset()
-            step = 1
-            while not self.env.is_terminal():
-                print('  Step %d' % step)
-                self.init()
-                state = self.env.cur_state()
-                action = self.behavior_policy.action(self.env, qf=self.qf)
-                reward, next_state = self.env.do_action(action)
-                self.init()
-                td_error = reward + (self.gamma * self.max_qvalue()) - self.qf[(state, action)]
-                self.qf[(state, action)] += self.alpha * td_error
-                step += 1
+        self.n_episode_ = 1
+        for _ in range(self.n_episodes):
+            self.episode()
+
+    def episode(self):
+        print('Episode %d / %d)' % (self.n_episode_, self.n_episodes))
+        self.env.reset()
+        step = 1
+        while not self.env.is_terminal():
+            print('  Step %d' % step)
+            self.init()
+            state = self.env.cur_state()
+            action = self.behavior_policy.action(self.env, qf=self.qf)
+            reward, next_state = self.env.do_action(action)
+            self.init()
+            td_error = reward + (self.gamma * self.max_qvalue()) - self.qf[(state, action)]
+            self.qf[(state, action)] += self.alpha * td_error
+            step += 1
+        self.n_episode_ += 1
