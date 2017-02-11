2719ba102bc4c44ea3e3fba6d8165c66473efe74
David Robles <drobles@gmail.com>
2017-02-11 13:10:06 -0500

    Update QLearningSelfPlay description

--- capstone/algorithms/qlearning_selfplay.py
+++ capstone/algorithms/qlearning_selfplay.py
@@ -6,7 +6,7 @@ class QLearningSelfPlay(QLearning):
     '''
     An specialization of the Q-learning algorithm that assumes
     a Game environment. In standard Q-learning the best action
-    is the one that leads the the maximum reward. In this version
+    is the one that selects the maximum reward. In this version
     we maximize for the first player of the game, and minimize
     for the second player.
     '''
