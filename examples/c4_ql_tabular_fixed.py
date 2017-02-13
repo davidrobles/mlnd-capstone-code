c88655858658883a440cc5a4f14d4cfb139705b3
David Robles <drobles@gmail.com>
2017-02-12 21:52:50 -0500

    Rename c4_qlearning_tabular to c4_ql_tabular_fixed

--- examples/c4_ql_tabular_fixed.py
+++ examples/c4_ql_tabular_fixed.py
@@ -4,7 +4,7 @@ position against a fixed Alpha-Beta opponent.
 '''
 from capstone.environment import Environment
 from capstone.game import Connect4
-from capstone.mdp import GameMDP
+from capstone.mdp import FixedGameMDP
 from capstone.player import AlphaBeta
 from capstone.rl import QLearning
 from capstone.util import c42pdf
@@ -16,12 +16,13 @@ board = [['X', 'O', 'X', 'O', ' ', ' ', ' '],
          ['X', 'O', 'X', 'O', 'X', 'O', 'X'],
          ['X', 'O', 'X', 'O', 'X', 'O', 'X']]
 game = Connect4(board)
-env = Environment(GameMDP(game, AlphaBeta(), 1))
+env = Environment(FixedGameMDP(game, AlphaBeta(), 1))
 qlearning = QLearning(env, n_episodes=1000)
 qlearning.learn()
 c42pdf('figures/c4_ql_current.pdf', game.board)
 
 for move in game.legal_moves():
+    print('*' * 80)
     value = qlearning.qf[(game, move)]
     print('Move: %s' % move)
     print('Value: %f' % value)
