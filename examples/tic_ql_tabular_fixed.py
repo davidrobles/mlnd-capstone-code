863be0e99a00cd446b19a5a884d97b65b2bbf516
David Robles <drobles@gmail.com>
2017-02-13 01:25:34 -0500

    Fix broken example tic_ql_tabular_fixed

--- examples/tic_ql_tabular_fixed.py
+++ examples/tic_ql_tabular_fixed.py
@@ -1,10 +1,10 @@
 '''
 Q-Learning is used to learn the state-action values for a
-Tic-Tac-Toe board position against a fixed Alpha-Beta opponent.
+Tic-Tac-Toe board position against a fixed Alpha-Beta opponent
 '''
 from capstone.environment import Environment
 from capstone.game import TicTacToe
-from capstone.mdp import GameMDP
+from capstone.mdp import FixedGameMDP
 from capstone.player import AlphaBeta
 from capstone.rl import QLearning
 from capstone.util import tic2pdf
@@ -13,12 +13,13 @@ board = [['X', ' ', ' '],
          ['O', 'X', ' '],
          [' ', 'O', ' ']]
 game = TicTacToe(board)
-env = Environment(GameMDP(game, AlphaBeta(), 1))
+env = Environment(FixedGameMDP(game, AlphaBeta(), 1))
 qlearning = QLearning(env, n_episodes=1000)
 qlearning.learn()
 tic2pdf('figures/tic_ql_current.pdf', game.board)
 
 for move in game.legal_moves():
+    print('*' * 80)
     value = qlearning.qf[(game, move)]
     print('Move: %d' % move)
     print('Value: %f' % value)
