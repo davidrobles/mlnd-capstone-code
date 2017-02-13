8ad5dac0d7112f3bb39ca709734ae5cde246cfc7
David Robles <drobles@gmail.com>
2017-02-13 01:21:30 -0500

    Delte tic_data_expl.py

--- examples/tic_data_expl.py
+++
@@ -1,7 +0,0 @@
-from capstone.game import TicTacToe
-from capstone.player import RandPlayer
-from capstone.util import play_series
-
-game = TicTacToe()
-players = [RandPlayer(), RandPlayer()]
-play_series(game, players)
