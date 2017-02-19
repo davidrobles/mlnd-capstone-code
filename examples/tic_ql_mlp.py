d324c5b1000581010214d5f89009927a23a798fc
David Robles <drobles@gmail.com>
2017-02-18 22:03:05 -0500

    Train a keras neural network via RL for TicTacToe

--- examples/tic_ql_mlp.py
+++ examples/tic_ql_mlp.py
@@ -1,47 +1,26 @@
-from keras.models import Sequential
-from keras.layers.core import Dense
-from keras.optimizers import sgd
-
 from capstone.environment import Environment
 from capstone.game import TicTacToe
+from capstone.mdp import GameMDP
+from capstone.player import AlphaBeta, RandPlayer
 from capstone.mdp import FixedGameMDP
-from capstone.player import AlphaBeta
-from capstone.rl import QLearning
-from capstone.utils import tic2pdf
+from capstone.rl import QLearning, MLP
+from capstone.utils import norm_tic_board
 
-class MLP(object):
-
-    def __getitem__(self, key):
-        return
-
-    def __setitem__(self, key, value):
-        dd = {'X': 1.0, ' ': 0.0, 'O': -1.0}
-        state, value = key
-        print(state)
-        pp = state.copy()
-        pp.make_move(value)
-        hey = [dd[col] for row in pp.board for col in row]
-        return
-
-board = [['X', ' ', ' '],
-         ['O', 'X', ' '],
-         [' ', 'O', ' ']]
+board = [[' ', ' ', ' '],
+         [' ', ' ', ' '],
+         [' ', ' ', ' ']]
 game = TicTacToe(board)
-env = Environment(FixedGameMDP(game, AlphaBeta(), 1))
-# qlearning = QLearning(env, qf=MLP(), n_episodes=1000)
-# qlearning.learn()
+mdp = GameMDP(game)
+env = Environment(FixedGameMDP(game, RandPlayer(), 1))
+mlp = MLP()
+qlearning = QLearning(env, qf=mlp, n_episodes=50000)
+qlearning.learn()
+mlp.model.save('models/qltic.h5')
 
-
-hello = MLP()
-hello[(game, 2)] = 12
-import numpy as np
-hidden_size = 50
-model = Sequential()
-# model.add(Dense(hidden_size, input_shape=(42,), activation='relu'))
-# model.add(Dense(hidden_size, activation='relu'))
-# model.add(Dense(1))
-model.add(Dense(1, input_dim=2))
-# model.add(Dense(1, input_shape=(1,)))
-model.compile(sgd(lr=.2), "mse")
-# print model.predict(np.array([1]))
-print(model.summary())
+for move in game.legal_moves():
+    print('-' * 80)
+    new_game = game.copy().make_move(move)
+    v = norm_tic_board(new_game)
+    value = mlp.model.predict(v, batch_size=1)
+    print(value[0][0])
+    print(new_game)
