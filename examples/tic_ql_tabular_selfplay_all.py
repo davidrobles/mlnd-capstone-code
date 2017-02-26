98206803a0d17eaf6b3801b03b8f3d614c6e0caf
David Robles <drobles@gmail.com>
2017-02-26 01:09:13 -0500

    Refactor tic_ql_tabular_selfplay_all

--- examples/tic_ql_tabular_selfplay_all.py
+++ examples/tic_ql_tabular_selfplay_all.py
@@ -7,7 +7,6 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from matplotlib.ticker import FuncFormatter
-import numpy as np
 from capstone.game.games import TicTacToe
 from capstone.game.players import GreedyQ, RandPlayer
 from capstone.game.utils import play_series, tic2pdf
@@ -16,23 +15,11 @@ from capstone.rl.learners import QLearningSelfPlay
 from capstone.rl.policies import RandomPolicy
 from capstone.rl.utils import Callback
 
-def to_percent(y, position):
-    # Ignore the passed in position. This has the effect of scaling the default
-    # tick locations.
-    s = str(100 * y)
-
-    # The percent symbol needs escaping in latex
-    if matplotlib.rcParams['text.usetex'] is True:
-        return s + r'$\%$'
-    else:
-        return s + '%'
-
-n_episodes = 60000
-
+n_episodes = 100000
 game = TicTacToe()
 env = Environment(GameMDP(game))
 
-class My(Callback):
+class Plotter(Callback):
 
     def __init__(self):
         self.x = []
@@ -41,39 +28,32 @@ class My(Callback):
         self.y_losses = []
 
     def on_episode_end(self, episode, qf):
-        if (episode == (n_episodes - 1)) or episode % 1000 == 0:
-            players = [GreedyQ(qf), RandPlayer()]
-            n_matches = 1000
-            results = play_series(TicTacToe(), players, n_matches=n_matches, verbose=0)
-            self.x.append(episode)
-            win_pct = results['W'] / n_matches
-            self.y_wins.append(win_pct)
-            draw_pct = results['D'] / n_matches
-            self.y_draws.append(draw_pct)
-            loss_pct = results['L'] / n_matches
-            self.y_losses.append(loss_pct)
+        if (episode != (n_episodes - 1)) and episode % 1000 != 0:
+            return
+        players = [GreedyQ(qf), RandPlayer()]
+        n_matches = 1000
+        results = play_series(TicTacToe(), players, n_matches=n_matches, verbose=0)
+        self.x.append(episode)
+        self.y_wins.append(results['W'] / n_matches)
+        self.y_draws.append(results['D'] / n_matches)
+        self.y_losses.append(results['L'] / n_matches)
 
     def on_train_end(self):
-        x = np.array(self.x)
-        y_wins = np.array(self.y_wins)
-        y_draws = np.array(self.y_draws)
-        y_losses = np.array(self.y_losses)
         fig = plt.figure()
         ax = fig.add_subplot(111)
-        w_line, = ax.plot(x, y_wins, label='Win')
-        d_line, = ax.plot(x, y_draws, label='Draw')
-        l_line, = ax.plot(x, y_losses, label='Loss')
+        w_line, = ax.plot(self.x, self.y_wins, label='Win')
+        d_line, = ax.plot(self.x, self.y_draws, label='Draw')
+        l_line, = ax.plot(self.x, self.y_losses, label='Loss')
         ax.set_xlim([0, n_episodes])
         ax.set_ylim([0, 1.0])
         plt.xlabel('Episodes')
-        plt.ylabel('Pct')
-        formatter = FuncFormatter(to_percent)
+        formatter = FuncFormatter(lambda y, pos: '{}%'.format(y * 100))
         plt.gca().yaxis.set_major_formatter(formatter)
         plt.legend(handles=[w_line, d_line, l_line])
-        plt.savefig('hey45.pdf')
+        plt.savefig('tic_ql_tabular_selfplay_all.pdf')
 
-
-qlearning = QLearningSelfPlay(env, policy=RandomPolicy(env.actions), learning_rate=0.1,
+behavior_policy = RandomPolicy(env.actions, random_state=23)
+qlearning = QLearningSelfPlay(env, policy=behavior_policy, learning_rate=0.1,
                               discount_factor=0.99, n_episodes=n_episodes, verbose=0,
-                              callbacks=[My()])
+                              callbacks=[Plotter()])
 qlearning.learn()
