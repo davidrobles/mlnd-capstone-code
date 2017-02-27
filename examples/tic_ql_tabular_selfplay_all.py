dc4989d7cb8840ba02cb4f6e364a6fddac250989
David Robles <drobles@gmail.com>
2017-02-26 20:10:46 -0500

    Remove unused EGreedy

--- examples/tic_ql_tabular_selfplay_all.py
+++ examples/tic_ql_tabular_selfplay_all.py
@@ -12,14 +12,12 @@ from capstone.rl.value_functions import TabularQ
 
 seed = 23
 game = TicTacToe()
-env = Environment(GameMDP(game))
-tabularq = TabularQ(random_state=seed)
-egreedy = EGreedy(env.actions, tabularq, epsilon=0.5, random_state=seed)
-rand_policy = RandomPolicy(env.actions, random_state=seed)
+mdp = GameMDP(game)
+env = Environment(mdp)
 qlearning = QLearningSelfPlay(
     env=env,
-    qfunction=tabularq,
-    policy=rand_policy,
+    qfunction=TabularQ(random_state=seed),
+    policy=RandomPolicy(env.actions, random_state=seed),
     learning_rate=0.1,
     discount_factor=0.99,
     n_episodes=65000,
