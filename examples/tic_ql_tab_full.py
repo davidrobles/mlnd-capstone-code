49570de44705019df3d78747c846c79fc4662357
David Robles <drobles@gmail.com>
2017-03-08 01:31:14 -0500

    Update tic_ql_tab_full

--- examples/tic_ql_tab_full.py
+++ examples/tic_ql_tab_full.py
@@ -1,6 +1,6 @@
 '''
 Q-Learning is used to estimate the state-action values for all
-Tic-Tac-Toe board position against a fixed Alpha-Beta opponent.
+Tic-Tac-Toe board position against a Random opponent.
 '''
 from capstone.game.games import TicTacToe
 from capstone.game.players import AlphaBeta, RandPlayer
@@ -8,12 +8,12 @@ from capstone.game.utils import tic2pdf
 from capstone.rl import FixedGameMDP, Environment
 from capstone.rl.learners import QLearning
 from capstone.rl.policies import RandomPolicy
-from capstone.rl.utils import QValuesPlotter
+from capstone.rl.utils import EpisodicWLDPlotter, QValuesPlotter
 from capstone.rl.value_functions import TabularQ
 
 seed = 23
 game = TicTacToe()
-mdp = FixedGameMDP(game, AlphaBeta(), 1)
+mdp = FixedGameMDP(game, RandPlayer(random_state=seed), 1)
 env = Environment(mdp)
 qlearning = QLearning(
     env=env,
@@ -21,14 +21,22 @@ qlearning = QLearning(
     policy=RandomPolicy(env.actions, random_state=seed),
     learning_rate=0.1,
     discount_factor=1.0,
-    n_episodes=800,
+    n_episodes=65000,
 )
 qlearning.train(
     callbacks=[
         QValuesPlotter(
             state=game,
             actions=game.legal_moves(),
+            period=100,
             filepath='figures/tic_ql_tab_full_qvalues_plot.pdf'
+        ),
+        EpisodicWLDPlotter(
+            game=game,
+            opp_player=RandPlayer(random_state=seed),
+            n_matches=1000,
+            period=1000,
+            filepath='figures/tic_ql_tab_full_wld_plot.pdf'
         )
     ]
 )
