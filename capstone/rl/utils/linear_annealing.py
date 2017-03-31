dabaac1b378502a9cc0e33ec564dc532433b743b
David Robles <drobles@gmail.com>
2017-03-31 17:27:33 -0400

    Remove print statements

--- capstone/rl/utils/linear_annealing.py
+++ capstone/rl/utils/linear_annealing.py
@@ -11,11 +11,9 @@ class LinearAnnealing(Callback):
         self.final = final
         self.n_episodes = n_episodes
         self.change_rate = (final - init) / n_episodes
-        print('change rate: %f' % self.change_rate)
 
     def on_episode_end(self, episode, qf):
         if ((self.doing == 'inc' and getattr(self.obj, self.param) < self.final) or
             (self.doing == 'dec' and getattr(self.obj, self.param) > self.final)):
                 prev = getattr(self.obj, self.param)
                 setattr(self.obj, self.param, prev + self.change_rate)
-                print('New epsilon: {}'.format(getattr(self.obj, self.param)))
