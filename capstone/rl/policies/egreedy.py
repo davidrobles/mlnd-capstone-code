6e3abf626520a865c9eb3af6317a5a2db0db31be
David Robles <drobles@gmail.com>
2017-04-01 12:30:45 -0400

    Set default epsilon

--- capstone/rl/policies/egreedy.py
+++ capstone/rl/policies/egreedy.py
@@ -6,7 +6,7 @@ from ...utils import check_random_state
 
 class EGreedy(Policy):
 
-    def __init__(self, provider, qf, epsilon, random_state=None):
+    def __init__(self, provider, qf, epsilon=0.1, random_state=None):
         self.provider = provider
         self.qf = qf
         self.epsilon = epsilon
