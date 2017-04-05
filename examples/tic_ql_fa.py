fed9cdb428c24dccc9c22a553321dc9feedc4b6d
David Robles <drobles@gmail.com>
2017-04-05 02:42:07 -0400

    Update experiment

--- examples/tic_ql_fa.py
+++ examples/tic_ql_fa.py
@@ -5,7 +5,7 @@ for the state-action values of Tic-Tac-Toe positions.
 from capstone.game.games import TicTacToe
 from capstone.game.players import RandPlayer
 from capstone.rl import Environment, GameMDP, FixedGameMDP
-from capstone.rl.learners import ApproximateQLearningSelfPlay, ApproximateQLearning
+from capstone.rl.learners import ApproximateQLearning
 from capstone.rl.policies import EGreedy
 from capstone.rl.utils import EpisodicWLDPlotter, Callback, LinearAnnealing
 from capstone.rl.value_functions import QNetwork
@@ -16,18 +16,18 @@ game = TicTacToe()
 # mdp = GameMDP(game)
 mdp = FixedGameMDP(game, RandPlayer(), 1)
 env = Environment(mdp)
-qnetwork = QNetwork(n_input_units=9, n_hidden_layers=1, n_output_units=9, n_hidden_units=100)
+qnetwork = QNetwork(mapping, n_input_units=9, n_hidden_layers=1, n_output_units=9, n_hidden_units=100)
 egreedy = EGreedy(env.actions, qnetwork, 1.0)
 qlearning = ApproximateQLearning(
     env=env,
     qfunction=qnetwork,
     policy=egreedy,
     discount_factor=1.0,
-    n_episodes=4000,
     experience_replay=True,
     batch_size=32
 )
 qlearning.train(
+    n_episodes=4000,
     callbacks=[
         EpisodicWLDPlotter(
             game=game,
