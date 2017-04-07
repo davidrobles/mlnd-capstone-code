977e11807955425dc2fdb3c405bd62278346ca49
David Robles <drobles@gmail.com>
2017-04-07 01:38:52 -0400

    Rename qf to qfunction

--- capstone/rl/policies/egreedy.py
+++ capstone/rl/policies/egreedy.py
@@ -6,13 +6,13 @@ from ...utils import check_random_state
 
 class EGreedy(Policy):
 
-    def __init__(self, provider, qf, epsilon=0.1, random_state=None):
+    def __init__(self, provider, qfunction, epsilon=0.1, random_state=None):
         self.provider = provider
-        self.qf = qf
+        self.qfunction = qfunction
         self.epsilon = epsilon
         self.random_state = check_random_state(random_state)
         self.rand = RandomPolicy(provider, self.random_state)
-        self.greedy = Greedy(provider, self.qf)
+        self.greedy = Greedy(provider, self.qfunction)
 
     def action(self, state):
         policy = self.rand if self.random_state.rand() < self.epsilon else self.greedy
