102a54e679816e4aa71d9d2e0205e09cd3d3545d
David Robles <drobles@gmail.com>
2017-04-08 02:23:22 -0400

    Add c4_dqn_uci.py

---
+++ examples/c4_dqn_uci.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from capstone.datasets import load_uci_c4 
+from capstone.game.games import Connect4 as C4
+
+def series_to_game(series):
+    '''Converts a Pandas Series to a Connect 4 game'''
+    cell_map = {'x': 'X', 'o': 'O', 'b': '-'}
+    board = [[' '] * C4.COLS for row in range(C4.ROWS)]
+    cells = series.iloc[:-1]
+    outcome = series.iloc[-1]
+    for ix, cell in enumerate(cells):
+        row = C4.ROWS - (ix % C4.ROWS) - 1
+        col = ix // C4.ROWS
+        board[row][col] = cell_map[cell]
+    return C4(board), outcome
+
+# Load UCI Connect 4 dataset
+df = load_uci_c4()
+
+# Select the instances of the dataset
+ixs = [1, 5, 10, 20, 567]
+
+# Generate pdfs of the boards
+for i in ixs:
+    row = df.iloc[i]
+    game, _ = series_to_game(row)
