2de8fd381e796443c1e0ef19f747825e5f78d296
David Robles <drobles@gmail.com>
2017-04-10 11:11:23 -0400

    Allow game callables in play_series

--- capstone/game/utils/play.py
+++ capstone/game/utils/play.py
@@ -21,14 +21,14 @@ def play_series(game, players, n_matches=100, verbose=True):
     """
     if verbose:
         print_header('Series')
-        print('Game:', game.name)
+        # print('Game:', game.name)
         print('Players:', players)
         print('No. Matches: %d\n' % n_matches)
     counters = {'W': 0, 'L': 0, 'D': 0}
     for n_match in range(1, n_matches + 1):
         if verbose:
             print('Match %d/%d:' % (n_match, n_matches), end=' ')
-        new_game = game.copy()
+        new_game = game() if callable(game) else game.copy()
         play_match(new_game, players, verbose=False)
         outcomes = new_game.outcomes()
         counters[outcomes[0]] += 1
