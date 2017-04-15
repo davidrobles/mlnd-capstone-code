d3ff18e2da5e4b9e13cfc23bcab43ff933ed6a21
David Robles <drobles@gmail.com>
2017-04-15 16:56:24 -0400

    this compiles at least...

--- capstone/rl/value_functions/qnetwork.py
+++ capstone/rl/value_functions/qnetwork.py
@@ -1,30 +1,65 @@
 # -*- coding: utf-8 -*-
 import numpy as np
-from keras.layers import Activation, Dense
+from keras.layers import Activation, Conv2D, Dense, Dropout, Flatten, MaxPooling2D
 from keras.models import Sequential
 from keras.optimizers import SGD
 from ..qfunction import QFunction
 from ...game.utils import normalize_board
 
 
+def convert_me(state):
+    def mapper(t):
+        if t == 'X':
+            return np.array([1.0])
+        elif t == 'O':
+            return np.array([-1.0])
+        else:
+            return np.array([0.0])
+    vfunc = np.vectorize(mapper)
+    robles = np.array(state.board)
+    x = vfunc(robles)
+    x = x.reshape(6, 7, -1)
+    return x
+
+
 class QNetwork(QFunction):
     '''A Q-Network is a neural network function approximator with θ weights.'''
 
+    # def __init__(self, mapping, n_input_units, n_hidden_layers, n_output_units,
+    #              n_hidden_units=10, learning_rate=0.01):
+    #     self.mapping = mapping
+    #     model = Sequential()
+    #     model.add(Convolution2D())
+    #     # input layer
+    #     model.add(Dense(n_hidden_units, input_shape=(n_input_units,)))
+    #     model.add(Activation('relu'))
+    #     # hidden layers
+    #     for _ in xrange(n_hidden_layers):
+    #         model.add(Dense(n_hidden_units))
+    #         model.add(Activation('relu'))
+    #     # output layer
+    #     model.add(Dense(n_output_units))
+    #     model.add(Activation('tanh'))
+    #     model.compile(loss='mse', optimizer=SGD(lr=learning_rate))
+    #     self.model = model
+
     def __init__(self, mapping, n_input_units, n_hidden_layers, n_output_units,
                  n_hidden_units=10, learning_rate=0.01):
+
         self.mapping = mapping
+        s = (6, 7, 1)
         model = Sequential()
-        # input layer
-        model.add(Dense(n_hidden_units, input_shape=(n_input_units,)))
-        model.add(Activation('relu'))
-        # hidden layers
-        for _ in xrange(n_hidden_layers):
-            model.add(Dense(n_hidden_units))
-            model.add(Activation('relu'))
-        # output layer
-        model.add(Dense(n_output_units))
+        model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=s))
+        model.add(Conv2D(64, (3, 3), activation='relu'))
+
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
+        model.add(Flatten())
+        model.add(Dense(128, activation='relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(7))
         model.add(Activation('tanh'))
-        model.compile(loss='mse', optimizer=SGD(lr=learning_rate))
+        model.compile(loss='mse', optimizer='rmsprop')
         self.model = model
 
     def minibatch_update(self, experiences, updates):
@@ -34,7 +69,9 @@ class QNetwork(QFunction):
         ylist = []
         for i in range(len(experiences)):
             state, action, reward, next_state = experiences[i]
-            x = normalize_board(state.board)
+            # import pdb; pdb.set_trace()
+            # x = normalize_board(state.board)
+            x = convert_me(state)
             xlist.append(x)
             y = self.model.predict(np.array([x]))
             a = self.mapping[action]
@@ -42,7 +79,7 @@ class QNetwork(QFunction):
             ylist.append(y[0])
         x = np.array(xlist)
         y = np.array(ylist)
-        self.model.fit(x, y, batch_size=batch_size, verbose=0)
+        self.model.train_on_batch(x, y)
 
     def update(self, state, action, value):
         x = normalize_board(state.board)
@@ -57,17 +94,29 @@ class QNetwork(QFunction):
 
     def __getitem__(self, state_action):
         state, action = state_action
-        x = normalize_board(state.board)
-        x = np.array([x])
-        value = self.model.predict(x, batch_size=1)
+        # x = normalize_board(state.board)
+        # x = np.array([x])
+        x = convert_me(state)
+        value = self.model.predict(np.array([x]), batch_size=1)
         a = self.mapping[action]
         assert isinstance(a, int)
         return value[0][a]
 
     def best_value(self, state, actions, max_or_min):
-        board = normalize_board(state.board)
-        x = np.array([board])
-        output = self.model.predict(x, batch_size=1)
+        # def mapper(t):
+        #     if t == 'X':
+        #         return np.array([1.0])
+        #     elif t == 'O':
+        #         return np.array([-1.0])
+        #     else:
+        #         return np.array([0.0])
+        # vfunc = np.vectorize(mapper)
+        # robles = np.array(state.board)
+        # x = vfunc(robles)
+        # x = x.reshape(6, 7, -1)
+        x = convert_me(state)
+        # import pdb; pdb.set_trace()
+        output = self.model.predict(np.array([x]))
         vals = []
         for aa in actions:
             hey = self.mapping[aa]
--- examples/c4_dqn_uci.py
+++ examples/c4_dqn_uci.py
@@ -12,7 +12,7 @@ import random
 
 move_mapper = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6}
 
-seed = 48
+seed = 84
 random.seed(seed)
 np.random.seed(seed)
 
@@ -22,27 +22,26 @@ env = Environment(mdp)
 qnetwork = QNetwork(
     move_mapper,
     n_input_units=42,
-    n_hidden_layers=1,
+    n_hidden_layers=3,
     n_output_units=7,
     n_hidden_units=400,
-    learning_rate=0.01
+    learning_rate = 0.001
 )
-# egreedy = EGreedy(
-#     action_space=env.actions,
-#     qfunction=qnetwork,
-#     epsilon=1.0,
-#     selfplay=False,
-#     random_state=seed
-# )
+egreedy = EGreedy(
+    action_space=env.actions,
+    qfunction=qnetwork,
+    epsilon=1.0,
+    selfplay=False,
+    random_state=seed
+)
 qlearning = ApproximateQLearning(
     env=env,
     qfunction=qnetwork,
-    # policy=egreedy,
-    policy=RandomPolicy(env.actions, random_state=seed),
+    policy=egreedy,
     discount_factor=0.99,
     selfplay=False,
     experience_replay=True,
-    replay_memory_size=10000,
+    replay_memory_size=1000,
     batch_size=32
 )
 class Monitor(Callback):
@@ -50,27 +49,28 @@ class Monitor(Callback):
         mdp = FixedGameMDP(get_random_game(), RandPlayer(random_state=seed), 1)
         env = Environment(mdp)
         qlearning.env = env
-        # egreedy.action_space = env.actions
+        egreedy.action_space = env.actions
         qlearning.policy.provider = env.actions
         if episode % 50 == 0:
             print('Episode {}'.format(episode))
 
-def hey():
-    qlearning.train(
-        n_episodes=10000,
-        callbacks=[
-            EpisodicWLDPlotter(
-                game=get_random_win_game,
-                opp_player=RandPlayer(random_state=seed),
-                n_matches=1000,
-                period=250,
-                filepath='figures/c4_dqn_uci.pdf'
-            ),
-            # LinearAnnealing(egreedy, 'epsilon', init=1.0, final=0.1, n_episodes=25000),
-            Monitor()
-        ]
-    )
+# prepopulate replay memory? read deepmind paper
 
-# import cProfile
-# cProfile.run('hey()')
-hey()
+qlearning.train(
+    n_episodes=100000,
+    callbacks=[
+        EpisodicWLDPlotter(
+            game=get_random_win_game,
+            opp_player=RandPlayer(random_state=seed),
+            n_matches=1000,
+            period=500,
+            filepath='figures/c4_dqn_uci.pdf'
+        ),
+        LinearAnnealing(egreedy, 'epsilon', init=1.0, final=0.1, n_episodes=10000),
+        Monitor()
+    ]
+)
+
+# got 90% with 42 input units, 3 hidden layers, 7 output units, 400 hidden units, lr=0.001, no
+# selfplay, only a 1,000 experience replay size, 100000 episodes, and linear annealing for 10,000
+# episodes only
