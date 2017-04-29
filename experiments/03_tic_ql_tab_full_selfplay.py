8a8b3323a5722230992b4c50cddfa692bb833aa3
David Robles <drobles@gmail.com>
2017-04-29 15:43:19 -0400

    updates

--- experiments/03_tic_ql_tab_full_selfplay.py
+++ experiments/03_tic_ql_tab_full_selfplay.py
@@ -6,24 +6,25 @@ from capstone.game.games import TicTacToe
 from capstone.game.players import RandPlayer
 from capstone.game.utils import tic2pdf
 from capstone.rl import Environment, GameMDP
-from capstone.rl.learners import QLearningSelfPlay
+from capstone.rl.learners import QLearning
 from capstone.rl.policies import RandomPolicy
 from capstone.rl.utils import EpisodicWLDPlotter
-from capstone.rl.value_functions import TabularQ
+from capstone.rl.value_functions import TabularVF
 
 seed = 23
 game = TicTacToe()
 mdp = GameMDP(game)
 env = Environment(mdp)
-qlearning = QLearningSelfPlay(
+qlearning = QLearning(
     env=env,
-    qfunction=TabularQ(random_state=seed),
-    policy=RandomPolicy(env.actions, random_state=seed),
+    qfunction=TabularVF(random_state=seed),
+    policy=RandomPolicy(action_space=env.action_space, random_state=seed),
     learning_rate=0.1,
     discount_factor=1.0,
-    n_episodes=70000
+    selfplay=True
 )
 qlearning.train(
+    n_episodes=70000,
     callbacks=[
         EpisodicWLDPlotter(
             game=game,
